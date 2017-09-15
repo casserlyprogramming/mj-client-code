128679bdc7875d19577fee7ceec2c5295440dbaa
Daniel Casserly <daniel@casserlyprogramming.com>
2017-09-15 11:34:22 +0100

    initial commit

---
+++ mj-client-code.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  Plugin Name: MJ Client Code
+  Plugin URI: https://github.com/casserlyprogramming/mj-client-code 
+  Description: Add a client code to the user screen in Wordpress 
+  Version: 1.0.0
+  Author: Daniel Casserly
+  Author URI: http://dandalfprogramming.blogspot.co.uk/
+ */
+
